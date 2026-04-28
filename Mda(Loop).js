ea8ad875217e2ac21185eaa460455f647a22c5bf
mdafiyankhan <khanmdyusuf117@gmail.com>
2026-04-28 20:03:22 +0530

    Mda.loop Done

---
+++ Mda(Loop).js
@@ -0,0 +1,29 @@
+// for Loop bsic code
+/*for(let i=0;i<9;i++){
+  console.log(i)// 0 t0 8 print hoga
+  console.log(i+1) // 1 to 9 print hoga
+}*/
+
+
+
+// for in loop bsaic code...... 
+
+/*let student ={  // 'Object' ek reserved keyword hai, isliye variable ka naam 'student' rakhna behtar hai
+  name : 9,
+  roll : 8,
+  section : 10
+};
+student = Number.parseInt(student)
+for(let x in student){
+  // Object[x] ka use karein
+  console.log("Detail of the student " +x+ " are " + student[x]);
+}*/
+
+
+
+// for of loop basic code.....
+/*for(let a of "afiyan"){
+  console.log(a)
+}*/
+
+//PRACTICLE PROGRAMME FOR LOOP 
---
+++ Mda.js
@@ -0,0 +1,52 @@
+/*let a = 8;
+let b = "8"`
+console.log(a>b && a==b)
+console.log(a>b && a===b)
+
+
+
+*/
+// this code is the logical operator of not operator which is different from && and tiled......
+
+/*let a = 8;
+let b = 6;
+console.log(!(a>b))*/
+// Conditonal statements..........
+
+/*let a = prompt("what is your age:");
+a = Number.parseInt(a);
+if(a>=18){
+  alert("you can drive but safely");
+}
+
+
+else if (a>15 && a<18){
+  alert("just wait for few years then you can drive ");
+}
+else{
+  alert("you can`t drive");
+}
+*/
+  
+// switch concept ............
+/*let day = prompt("select your day using nuymber:");
+day = Number.parseInt(day);
+switch(day){
+  case 1:
+  console.log("Monday")
+  break;
+  
+  case 2:
+  console.log("Tuesday")
+  break;
+  
+  case 3:
+  console.log("Sunday")
+  break;
+  default:
+  
+  console.log("Invalid Day")
+
+}*/
+
+  
